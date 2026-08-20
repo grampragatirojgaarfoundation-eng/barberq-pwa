d7c61214d010dd731c2f6fbe7a3c9b5ddecefbda
grampragatirojgaarfoundation-eng <grampragatirojgaarfoundation@gmail.com>
2026-08-21 00:26:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ const io = new Server(server, { cors: { origin: '*' } });
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
+
+// Main page (Home Page) ke liye direct route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Storage Engine for Salon Photo Uploads
